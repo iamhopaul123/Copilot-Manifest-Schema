b65fc7d486c6959f0e3cf9c406600b369a8a0cbe
Aiden Carpenter <72675057+CaptainCarpensir@users.noreply.github.com>
2022-07-20 17:03:17 -0600

    feat: allow rate limit bypass with token (#12)
    * add error handling for api rate limit
    
    * feat: allow rate limit bypass with token

--- doc_retriever.py
+++ doc_retriever.py
@@ -1,16 +1,28 @@
 import requests
 import os
 
-def retrieve_docs():
+def retrieve_docs(username="", token=""):
     base_gh_url = "https://api.github.com/repos/aws/copilot-cli/contents/site/content/docs"
-    main_resp = requests.get(os.path.join(base_gh_url, "manifest"))
+    if username != "" and token != "":
+        main_resp = requests.get(os.path.join(base_gh_url, "manifest"), auth=(username,token))
+    else:
+        main_resp = requests.get(os.path.join(base_gh_url, "manifest"))
+
+    if "message" in main_resp.json():
+        raise Exception(main_resp.json()["message"])
+
     main_docs = {}
     for resp in main_resp.json():
         if not resp["name"].endswith(".en.md"):
             continue
         r = requests.get(resp["download_url"])
         main_docs[resp["name"].rstrip(".md")] = r.text
-    partial_resp = requests.get(os.path.join(base_gh_url, "include"))
+    if username != "" and token != "":
+        partial_resp = requests.get(os.path.join(base_gh_url, "include"), auth=(username,token))
+    else:
+        partial_resp = requests.get(os.path.join(base_gh_url, "include"))
+    if "message" in partial_resp.json():
+        raise Exception(partial_resp.json()["message"])
     partial_docs = {}
     for resp in partial_resp.json():
         if not resp["name"].endswith(".en.md"):
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import json
 from doc_parser import doc_parser
 from doc_retriever import retrieve_docs
 from generator import generate_schema
+import sys
 
 def main():
-    docs = retrieve_docs()
+    args = sys.argv[1:]
+    username = ""
+    token = ""
+    if len(args) == 2:
+        username = args[0]
+        token = args[1]
+    docs = retrieve_docs(username, token)
     parser = doc_parser()
     for k, v in sorted(docs["main"].items()):
         parser.appendToMain(k, v)
