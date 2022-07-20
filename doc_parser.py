ea504812059c4893ba43cb1f12834f1dba646225
penghaoh <penghaoh@amazon.com>
2022-07-20 13:25:30 -0700

    Add main and wrap up

--- doc_parser.py
+++ doc_parser.py
@@ -2,10 +2,30 @@ import marko
 import html
 import re
 from bs4 import BeautifulSoup as bs, NavigableString, Tag
+import os
+import json
 
-def readFileAsString(fileName: str):
-    file = open(fileName)
-    return file.read()
+class doc_parser:
+    def __init__(self):
+        self._weblink = "https://aws.github.io/copilot-cli/docs"
+        self._json_dic = {
+            "main": [],
+            "partial": []
+        }
+    
+    def parsed_doc(self):
+        return json.dumps(self._json_dic)
+
+    def appendToMain(self, file_name, content):
+        return self._appendToJsonDict("main", content, file_name, os.path.join(self._weblink, "manifest"))
+    
+    def appendToPartial(self, file_name, content):
+        return self._appendToJsonDict("partial", content, file_name, os.path.join(self._weblink, "include"))
+        
+    def _appendToJsonDict(self, key, content, file_name, _weblink):
+        struct = parseMdToJson(content, file_name, _weblink)
+        if len(struct["properties"]) != 0:
+            self._json_dic[key].append(struct)
 
 def preProcess(inStream: str):
     regexQuery = '`{1,3}[^`]+`{1,3}'
@@ -68,7 +88,7 @@ def parseMdToJson(inStream: str, fileName: str, weblink: str):
                 {
                     "field": ancestors + field,
                     "type": type,
-                    "description": "Type: " + type + "\n" + description + "\nMore info: " + weblink + fileName.split('.')[0] + "/" + href,
+                    "description": "Type: " + type + "\n" + description + "\nMore info: " + os.path.join(weblink, fileName.split('.')[0], href)
                 }
             )
         if "% include" in description:
